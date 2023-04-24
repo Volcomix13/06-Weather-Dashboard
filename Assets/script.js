2191de0fd1ccc3a101506c08e10359c54a06f772
Volcomix <m.santo13@hotmail.com>
2023-04-23 19:32:41 -0600

    Added fetch code to js

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,15 @@
 var APIKey = "93f9d7b6a911ae94c5877f3de5caa46e";
-var city;
+var city = "El Paso" //need to make empty string
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-fetch (queryURL)
+fetch (queryURL, {
+    method: "GET"
+})
+    .then(function(resonse) {
+        return resonse.json();
+})
+    .then(function(data) {
+        console.log(data);
+})
+
+
