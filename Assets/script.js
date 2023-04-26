33804a7a5de8234fbb9b23d93c127e813d8eacf1
Volcomix <m.santo13@hotmail.com>
2023-04-25 22:10:09 -0600

    added function, event listener and loop. Needs debugging

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,44 @@
 var APIKey = "93f9d7b6a911ae94c5877f3de5caa46e";
-var city = "El Paso";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var inputCity = document.querySelector("#city-name");
 
+var searchForm = document.querySelector(".btn");
+var weatherData = document.querySelector(".card-body");
 
+var cityName = document.querySelector(".city-name and date"); //can I do this? LOL
+cityName.textContent = inputCity;
 
 
 //grabs data from openweathermap
+ 
+function showWeather(event){
+    event.preventDefault();
+    
+  
+    var city = inputCity.value;
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
+        fetch (queryURL)
+            .then(function(response) {
+                return response.json();
+        })
+            .then(function(data) {
+                console.log(data);
+                for(i=0; i<6; i++){ //loop to display 6 days of data
+                    var tempElement = document.querySelector(".temperature1" +  (i+1));
+                    console.log(data.list[i*8]);
+                    var temp1 = data.list[i*8].main.temp;
+                    tempElement.textContent = "temperature1" + temp1;
 
-fetch (queryURL, {
-    method: "GET"
-})
-    .then(function(resonse) {
-        return resonse.json();
-})
-    .then(function(data) {
-        console.log(data);
-})
-showWeather();
+                    var humidityElement = document.querySelector(".humidity" + (i+1));
+                    var humi1 = data.list[i*8].main.humidity;
+                    humidityElement.textContent = "humidity1" + humi1;
 
-function showWeather (resonse){
-    console.log(resonse);
-    var row = document.querySelector(".card");
-    var html = `<img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="city-name and date">City Name</h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                 <li class="temperature">Temperature</li>
-                    <li class="humidity">Humidity</li>
-                    <li class="wind-speed">Wind Speed</li>
-                </ul>`;
-}
+                    
+
+                }
+
+        })
+};
+
+searchForm.addEventListener("click", showWeather);
+
+
