93bbf6e5f3b34e93d4f91a35e6f3a34042fd1e2c
Volcomix <m.santo13@hotmail.com>
2023-04-26 22:51:32 -0600

    debugged data returns for fetch function

--- Assets/script.js
+++ Assets/script.js
@@ -4,9 +4,6 @@ var inputCity = document.querySelector("#city-name");
 var searchForm = document.querySelector(".btn");
 var weatherData = document.querySelector(".card-body");
 
-var cityName = document.querySelector(".city-name and date"); //can I do this? LOL
-cityName.textContent = inputCity;
-
 
 //grabs data from openweathermap
  
@@ -15,6 +12,7 @@ function showWeather(event){
     
   
     var city = inputCity.value;
+
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
         fetch (queryURL)
             .then(function(response) {
@@ -22,17 +20,24 @@ function showWeather(event){
         })
             .then(function(data) {
                 console.log(data);
-                for(i=0; i<6; i++){ //loop to display 6 days of data
-                    var tempElement = document.querySelector(".temperature1" +  (i+1));
+                for(i=0; i<5; i++){ //loop to display 6 days of data
+                    var cityName = document.querySelector(".city-name");
+                    cityName.textContent = city;
+
+                    var tempElement = document.querySelector(".temperature" +  (i+1));
                     console.log(data.list[i*8]);
                     var temp1 = data.list[i*8].main.temp;
-                    tempElement.textContent = "temperature1" + temp1;
+                    tempElement.textContent = "Temperature: " + temp1 + "°F";
 
                     var humidityElement = document.querySelector(".humidity" + (i+1));
                     var humi1 = data.list[i*8].main.humidity;
-                    humidityElement.textContent = "humidity1" + humi1;
+                    humidityElement.textContent = "Humidity: " + humi1 + "%";
 
-                    
+                   var windSpeed = document.querySelector(".wind-speed" + (i+1));
+                   var wind1 = data.list[i*8].wind.speed;
+                   windSpeed.textContent = "Wind Speed: " + wind1 + " mi/hr";
+
+                   
 
                 }
 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 06-Weather-Dashboard
+
+## Credits
+Background Photo: Photo by <a href="https://unsplash.com/@lukaszlada?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Łukasz Łada</a> on <a href="https://unsplash.com/photos/LtWFFVi1RXQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
